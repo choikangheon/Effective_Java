bf7a15cfaa34571d9aec11b0ce135d93f9990a2a
choikangheon <kangheon3@gmail.com>
2022-11-01 22:37:34 +0900

    [init] 2022-11-01 이펙티브 자바 스터디

---
+++ src/main/java/org/example/chapter01/Order.java
@@ -0,0 +1,21 @@
+package org.example.chapter01;
+
+public class Order {
+    private boolean prime;
+    private boolean urgent;
+    private Product product;
+
+    public static Order primeOrder(Product product){
+        Order order = new Order();
+        order.prime = true;
+        order.product = product;
+        return order;
+    }
+
+    public static Order urgentOrder(Product product){
+        Order order = new Order();
+        order.prime = true;
+        order.product = product;
+        return order;
+    }
+}
