c90822eaf8bee776476abf469093da5125f4365a
choikangheon <52992334+choikangheon@users.noreply.github.com>
2022-11-15 23:44:06 +0900

    2022-11-15 가변인수, 불필요한 객체

---
+++ src/main/java/org/example/chapter01/item06/Strings.java
@@ -0,0 +1,13 @@
+package org.example.chapter01.item06;
+
+public class Strings {
+    public static void main(String[] args) {
+        String hello = "hello";
+//        String hello2 = new String("hello");
+        String hello3 = "hello";
+
+        System.out.println(hello.equals(hello3));
+    }
+
+
+}
